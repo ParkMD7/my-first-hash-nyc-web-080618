bb803325fee2d731622fd222bb93ffff626141e8
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-25 17:16:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -43,8 +43,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
+    shipping_manifest["hometown"] = "Massena, NY"
 
   # add 4 gun powder to the shipping_manifest hash below
 
